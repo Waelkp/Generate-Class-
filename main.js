c04f2753c0fa84cc563e7fe1e64538c7b46dba7a
Waelkp <waelkp187@gmail.com>
2024-01-29 04:20:13 +0300

    first stage

---
+++ main.js
@@ -0,0 +1,41 @@
+// Get Elements From Html
+let add = document.querySelector(".classes-to-add");
+let remove = document.querySelector(".classes-to-remove");
+let list = document.querySelector(".classes-list");
+let listDiv = document.querySelector(".classes-list div");
+let cDiv = document.querySelector("[title='Current']");
+
+// Action
+add.onblur = function (e) {
+  let ch = add.value.split(" ");
+  ch.sort();
+  for (let i = 0; i < ch.length; i++) {
+    if (ch[i] == "") {
+      continue;
+    }
+    let element = ch[i];
+    let mySpan = document.createElement("span");
+    mySpan.textContent = element.toLowerCase();
+    // cDiv.className = add.value;
+    cDiv.classList.add(ch[i]);
+    console.log(cDiv.classList);
+    listDiv.appendChild(mySpan);
+  }
+  add.value = "";
+};
+remove.onblur = function (e) {
+  let del = remove.value.toLowerCase().split(" "); // make array
+  console.log(listDiv.children);
+  console.log(del);
+  for (let x = 0; x < del.length; x++) {
+    if (del[x] == "") {
+      continue;
+    }
+    for (let i = 0; i < listDiv.children.length; i++) {
+      if (del[x] === listDiv.children[i].textContent)
+        listDiv.children[i].remove();
+      // cDiv.classList.remove(del[x]);
+    }
+  }
+  remove.value = "";
+};
